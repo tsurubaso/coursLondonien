ba29ddff8b9e5257a911fa20d13dfb724344b711
tsurubaso <tsurubaso@gmail.com>
2023-04-27 09:02:38 +0900

    Premier commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+	
+	public List <Student> getStudents() {
+        return List.of( new Student(1L, "Mariam","Mariam@gmail.com", LocalDate.of(2000, Month.JANUARY, 05),21));
+    }
+
+}
